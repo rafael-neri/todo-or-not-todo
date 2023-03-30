5f6a1f00e8e48974022687fbe3b733dfb821d82f
Rafael Neri <rafepel@gmail.com>
2023-03-29 23:34:25 -0300

    Update Style

--- src/components/item/index.jsx
+++ src/components/item/index.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export function Item({ title, checked, children }) {
     return (
         <li className={checked ? 'checked' : ''} >
-            <div>{title}</div>
+            <div style={checked ? { textDecoration: 'line-through' } : {}}>
+                {title}
+            </div>
             {children}
         </li>
     )
